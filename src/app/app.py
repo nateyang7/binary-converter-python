e60a2e87549d4b4ed3f8cb4aa4a6d7baae37a22f
Nathan Yang <nathanyang.w@outlook.com>
2025-03-24 02:12:23 +0100

    Upd cls App

--- src/app/app.py
+++ src/app/app.py
@@ -9,22 +9,35 @@ class App(tk.Tk):
 
     def __init__(self) -> None:
         tk.Tk.__init__(self)
-        self.geometry("700x700")
+        #self.geometry("700x700")
         self.info_label: tk.Frame = tk.Frame(self, bg=self.BACKGROUND_COLOR)
-        self.info_label.grid(row=0, column=0, sticky='we')
+        #self.info_label.grid(row=0, column=0, sticky='we')
+        self.info_label.pack(fill='x')
         self.label: tk.Label = tk.Label(
                 self.info_label, text="Enter two numbers", 
                 bg=self.BACKGROUND_COLOR, fg=self.FOREGROUND_COLOR
                 )
         self.label.pack(fill='x')
-        self.buttons_frame: tk.Frame = tk.Frame(self)
-        self.buttons_frame.grid(row=1, column=1)
+        self.entry: tk.Entry = tk.Entry(self, bg='grey')
+        self.entry.pack()
+        self.buttons_frame: tk.Frame = tk.Frame(self, bg="blue")
+        self.buttons_frame.pack(side="bottom", fill='x')
+        #self.buttons_frame.grid(row=1, column=1)
+        self.setup_buttons(4, 4)
         self.center_window()
         self.mainloop()
 
-    def setup_label(self) -> None:
-        """ Créer la frame pour le label """
-        pass
+    def setup_buttons(self, rows: int, cols: int) -> None:
+        """ Créer et organise la frame des boutons """
+        buttons_grid: list[list[int]] = [
+            [0] * cols for _ in range(rows)
+        ]
+        for row in range(rows):
+            for column in range(cols):
+                print(row, column)
+                button: tk.Button = tk.Button(self.buttons_frame, text='x')
+                buttons_grid[row][column] = button
+                buttons_grid[row][column].grid(row=row, column=column)
 
     def center_window(self) -> None:
         """ Centre la fenêtre principale """
