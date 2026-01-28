9a36136c57900d369a62d1717e1a5d97a81ea828
Nathan Yang <nathanyang.w@outlook.com>
2026-01-28 11:42:05 +0100

    Finish function to convert integer to binary

--- binary_converter.py
+++ binary_converter.py
@@ -8,10 +8,22 @@ TO-DO:
 class BinaryConverter:
     """
     Class representing a binary converter.
+
+    Attributes:
+        n (int): Integer to convert. (DEFAULT: 0)
     """
 
     def __init__(self) -> None:
-        ...
+        """
+        Initializes the binary converter.
+        """
+        self.n = 0
+
+    def get_integer(self) -> int:
+        return self.n
+
+    def set_integer(self, new_integer: int) -> None:
+        self.n = new_integer
 
     def bin(self) -> str:
         """
@@ -23,8 +35,13 @@ class BinaryConverter:
         Examples:
             >>>
         """
-        ...
-
+        bits: str = ''
+        n: int = self.get_integer()
+        while n / 2 != 0:
+            bits += str(n % 2)
+            n //= 2
+        return bits[::-1]
+        
     def oct(self) -> str:
         """
         Converts an integer to a octal notation.
@@ -51,3 +68,4 @@ class BinaryConverter:
 
     def __repr__(self) -> str:
         ...
+
