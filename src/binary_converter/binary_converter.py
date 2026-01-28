22bc6c75f79644dd2d50cb7b20b68080aff55f82
Nathan Yang <nathanyang.w@outlook.com>
2026-01-28 11:20:15 +0100

    Add basic structures to binary converter

---
+++ src/binary_converter/binary_converter.py
@@ -0,0 +1,53 @@
+"""
+Author: Nathan Yang
+
+TO-DO:
+    - [] Can convert integers with two's complement
+"""
+
+class BinaryConverter:
+    """
+    Class representing a binary converter.
+    """
+
+    def __init__(self) -> None:
+        ...
+
+    def bin(self) -> str:
+        """
+        Converts an integer to binary.
+
+        Returns:
+            str: String of the integer converted to binary.
+
+        Examples:
+            >>>
+        """
+        ...
+
+    def oct(self) -> str:
+        """
+        Converts an integer to a octal notation.
+
+        Returns:
+            str: String of the integer converted to octal notation.
+
+        Examples:
+            >>>
+        """
+        ...
+
+    def hex(self) -> str:
+        """
+        Converts an integer to a hex number.
+
+        Returns:
+            str: String of the integer converted hexadecimal.
+
+        Examples:
+            >>>
+        """
+        ...
+
+    def __repr__(self) -> str:
+        ...
