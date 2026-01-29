95455c7f6ffb5ca078089860004fef4e278bf1b4
Nathan Yang <nathanyang.w@outlook.com>
2026-01-29 10:14:36 +0100

    Change bits to list[int]

--- src/binary_converter/binary_converter.py
+++ src/binary_converter/binary_converter.py
@@ -10,32 +10,32 @@ class BinaryConverter:
     Class representing a binary converter.
 
     Attributes:
-        n (int): Integer to convert. (DEFAULT: 0)
+        __n (int): Integer to convert. (DEFAULT: 0)
     """
 
     def __init__(self) -> None:
         """
         Initializes the binary converter.
         """
-        self.n = 0
+        self.__n = 0
 
     def get_integer(self) -> int:
-        return self.n
+        return self.__n
 
     def set_integer(self, new_integer: int) -> None:
-        self.n = new_integer
+        self.__n = new_integer
 
-    def nat_bin(self) -> str:
+    def nat_bin(self) -> list[int]:
         """
         Converts a natural integer to binary.
 
         Returns:
             str: String of the natural integer converted to binary
         """
-        bits: str = ''
+        bits: str = []
         n: int = self.get_integer()
         while n / 2 != 0:
-            bits += str(n % 2)
+            bits.append(n % 2)
             n //= 2
         return bits[::-1]
 
--- tests/test_binary_converter.py
+++ tests/test_binary_converter.py
@@ -5,13 +5,13 @@ from binary_converter.binary_converter import BinaryConverter
 def binary_converter():
     return BinaryConverter()
 
-def test_set_integer(binary_converter):
+def test_set_integer(binary_converter: BinaryConverter):
     binary_converter.set_integer(10)
     assert binary_converter.get_integer() == 10
 
-def test_natural_integer_to_binary(binary_converter):
+def test_natural_integer_to_binary(binary_converter: BinaryConverter):
     binary_converter.set_integer(10)
-    assert binary_converter.nat_bin() == "1010"
+    assert binary_converter.nat_bin() == [1, 0, 1, 0]
 
 """
 def test_integer_to_binary(binary_converter):
