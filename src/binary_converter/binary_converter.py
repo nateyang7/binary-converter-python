55fe85afe4bbc04ba05a4d52752d6fbf8ee9b78b
Nathan Yang <nathanyang.w@outlook.com>
2026-02-16 23:16:43 +0100

    Remove methods and focus only on natural integers

--- src/binary_converter/binary_converter.py
+++ src/binary_converter/binary_converter.py
@@ -29,49 +29,7 @@ class BinaryConverter:
     def set_bits(self, new_bits: list[int]) -> None:
         self.__bits = new_bits
 
-    def nat_bin(self) -> list[int]:
-        """
-        Converts a natural integer to binary.
-
-        Returns:
-            str: String of the natural integer converted to binary
-
-        Examples:
-            >>> b = BinaryConverter()
-            >>> b.set_integer(10)
-            >>> b.nat_bin()
-            [1, 0, 1, 0]
-        """
-        bits: str = []
-        n: int = self.get_integer()
-        while n / 2 != 0:
-            bits.append(n % 2)
-            n //= 2
-        return bits[::-1]
-
-    def reverse_bits(self) -> list[int]:
-        """
-        Reverse bits from a binary number.
-
-        Returns:
-            None.
-        """
-        for bit in self.__bits():
-            bit = 0 if bit == 1 else 1
-
-    def add(self, bits: list[int]) -> None:
-        """
-        Add bits to the current binary number.
-
-        Args:
-            bits (list[int]): List of bits of another number.
-
-        Returns:
-            None.
-        """
-        ...
-
-    def bin(self, bits: int = 8) -> str:
+    def bin(self) -> str:
         """
         Converts an integer to binary depending on the number of bits.
 
@@ -80,17 +38,16 @@ class BinaryConverter:
 
         Examples:
             >>> b = BinaryConverter()
-            >>> b.set_integer(-10)
+            >>> b.set_integer(10)
             >>> b.bin()
-            [1, 1, 1, 1, 0, 1, 1, 0]
+            '1010'
         """
-        if self.get_integer() >= 0:
-            return self.nat_bin()
-        bits_list: list[int] = self.nat_bin()
-        while len(bits_list) != bits:  # Fill bits to get the size required
-            bits_list.insert(0)
-        self.set_bits(bits_list)
-        self.reverse_bits()
+        n: int = self.get_integer()
+        bits: list[int] = []
+        while n / 2 != 0:
+            bits.append(n % 2)
+            n //= 2
+        return ''.join(str(bit) for bit in bits[::-1])
         
     def oct(self) -> str:
         """
@@ -100,7 +57,10 @@ class BinaryConverter:
             str: String of the integer converted to octal notation.
 
         Examples:
-            >>>
+            >>> b = BinaryConverter()
+            >>> b.set_integer(255)
+            >>> b.oct()
+            '377'
         """
         ...
 
@@ -112,7 +72,10 @@ class BinaryConverter:
             str: String of the integer converted hexadecimal.
 
         Examples:
-            >>>
+            >>> b = BinaryConverter()
+            >>> b.set_integer(26)
+            >>> b.hex()
+            '1A'
         """
         ...
 
--- tests/test_binary_converter.py
+++ tests/test_binary_converter.py
@@ -9,22 +9,24 @@ def test_set_integer(binary_converter: BinaryConverter):
     binary_converter.set_integer(10)
     assert binary_converter.get_integer() == 10
 
+"""
 def test_natural_integer_to_binary(binary_converter: BinaryConverter):
     binary_converter.set_integer(10)
     assert binary_converter.nat_bin() == [1, 0, 1, 0]
-
-"""
-def test_integer_to_binary(binary_converter):
-    binary_converter.set_integer(-10)
-    ...
 """
 
-def test_oct(binary_converter: BinaryConverter):
+def test_bin(binary_converter):
     binary_converter.set_integer(10)
-    assert binary_converter.oct() == '0o12'
+    assert binary_converter.bin() == '1010'
 
-
+"""
 def test_oct(binary_converter: BinaryConverter):
-    binary_converter.set_integer(26)
-    assert binary_converter.hex() == '0x1A'
+    binary_converter.set_integer(255)
+    assert binary_converter.oct() == '377'
+"""
 
+"""
+def test_hex(binary_converter: BinaryConverter):
+    binary_converter.set_integer()
+    assert binary_converter.hex() == '1A'
+"""
